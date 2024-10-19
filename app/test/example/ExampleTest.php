3a85a6fab2cb3d93aa7a34c4f9182ceaf29e62f3
Tomas Nania <tnania2022@gmail.com>
2024-10-19 16:45:12 -0300

    volviendo a la normalidad

---
+++ app/test/example/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+final class ExampleTest extends TestCase
+{
+    public function testShouldCheckAssertTrue(){
+        $this->assertTrue(true);
+    }
+    public function testShouldCheckAssertEquals(){
+        $this->assertEquals("a", "a");
+    }
+}
