77ad996c4263a6b66cc1c4bcc8b6531376739b57
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2024-10-21 13:02:38 -0300

    validar correo falso

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -1,5 +1,7 @@
 <?php
 
+use JetBrains\PhpStorm\NoReturn;
+
 class LoginController
 {
     private $usuarioModelo;
@@ -24,17 +26,24 @@ class LoginController
         $this->presenter->show("login", $data);
     }
 
-    public function validar()
+    public function validar(): void
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $user = $_POST['username'];
             $pass = $_POST['password'];
 
+            // Obtener usuario de la DB
             $usuario = $this->usuarioModelo->getUsuarioPorUsername($user);
             if ($usuario && password_verify($pass, $usuario['password'])) {
+                // Buscamos si esta verificado
+                $usuario["verificado"] =
+                    $this->usuarioModelo->getVerificacionDeUsuario($usuario["id"])["verificado"];
+                // Guardamos usuario en sesión
                 $_SESSION['usuario'] = $usuario;
+                // A casita perro
                 $this->redireccionar("home");
             } else {
+                // No se encontró usuario
                 $_SESSION["error"] = "Credenciales incorrectas";
                 $this->redireccionar("login");
             }
@@ -48,7 +57,7 @@ class LoginController
      * @param $ruta
      * @return void
      */
-    private function redireccionar($ruta)
+    #[NoReturn] private function redireccionar($ruta): void
     {
         header("Location: /PW2-preguntones/$ruta");
         exit();
--- controller/RegistroController.php
+++ controller/RegistroController.php
@@ -27,49 +27,90 @@ class RegistroController
         $this->presenter->show("registro", $data);
     }
 
+
     // Registrar
-    public function registrar()
+    public function registrar(): void
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $usuario = [
-                'nombre' => $_POST['nombre'],
-                'apellido' => $_POST['apellido'],
-                'username' => $_POST['username'],
-                'email' => $_POST['email'],
-                'password' => $_POST['password'],
-                'anio_nacimiento' => $_POST['anio_nacimiento'],
-                'id_sexo' => $_POST['id_sexo'],
-                'id_ciudad' => $_POST['id_ciudad']
-            ];
+            // Obtener información del formulario
+            $usuario = $this->obtenerDatosDelUsuario();
 
+            // Registrar usuario
             $resultadoUsuario = $this->usuarioModel->registrarUsuario($usuario);
 
+            // Manejar error
             if (!$resultadoUsuario['success']) {
-//                $data["error"] = RegistroModel::ERROR_REGISTRO_USUARIO;
                 $data["error"] = "Algo salio mal.";
-//            require 'view/registroView.mustache';
                 $this->presenter->show("registro", $data["error"]);
                 return;
             }
 
-//            $idUsuario = $this->usuarioModel->getUltimoIdGenerado();
+            // Guardamos jugador
+            $result = $this->usuarioModel->guardarJugador();
 
-            $_SESSION["success"] = "Te registraste bien, ahora logueate capo.";
-            header("Location: /PW2-Preguntones/login");
-            exit();
-//            $this->presenter->show("home", $data["success"]);
-//            require 'view/registro.success.mustache';
+            // Manejar error
+            if ($result["success"]) {
+                $_SESSION["correoParaValidar"] = $usuario['email'];
+                $_SESSION["usuario"] = $usuario;
+                $this->redireccionar("registro/validarCorreo");
+            } else {
+                $this->presenter->show("error", []);
+            }
+        } else $this->redireccionar("registro");
+    }
+
+    public function validarJugador()
+    {
+        $id = $_POST["id_usuario"];
+        $result = $this->usuarioModel->validarJugador($id);
+        if ($result["success"]) {
+            $_SESSION['usuario']["verificado"] = true;
+            $this->redireccionar("home");
         } else {
-            require 'view/registroView.mustache';
-
+            $data = ["error" => "Algo salió mal"];
+            $this->presenter->show("error", $data);
         }
     }
 
+    public function validarCorreo()
+    {
+        $data = [
+            "correoParaValidar" => $_SESSION["correoParaValidar"],
+            "id_usuario" => $_SESSION["id_usuario"],
+        ];
+        $this->presenter->show("validacionCorreo", $data);
+    }
+
+
     public function getCiudades()
     {
-
         $id = $_GET["id"];
         $variablesCiudades = $this->paisesModel->getCiudades($id);
         echo json_encode($variablesCiudades);
     }
+
+    private function obtenerDatosDelUsuario(): array
+    {
+        return [
+            'nombre' => $_POST['nombre'],
+            'apellido' => $_POST['apellido'],
+            'username' => $_POST['username'],
+            'email' => $_POST['email'],
+            'password' => $_POST['password'],
+            'anio_nacimiento' => $_POST['anio_nacimiento'],
+            'id_sexo' => $_POST['id_sexo'],
+            'id_ciudad' => $_POST['id_ciudad']
+        ];
+    }
+
+    /**
+     * @param $ruta
+     * @return void
+     */
+    #[NoReturn] private function redireccionar($ruta): void
+    {
+        header("Location: /PW2-preguntones/$ruta");
+        exit();
+    }
+
 }
